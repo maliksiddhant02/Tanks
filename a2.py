9e96b4aacb5ae39364f7618dc36bf90b0d0cfd3b
maliksiddhant02 <maliksiddhant02@gmail.com>
2025-10-11 15:01:29 +1000

    first 3 tasks completed

--- a2.py
+++ a2.py
@@ -15,3 +15,27 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+class Tile:
+    def __init__(self, tile_id=TILE_ID, blocking=False):
+        self.tile_id = tile_id
+        self.blocking = blocking
+
+    def __str__(self) -> str:
+        return self.tile_id
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+    def is_blocking(self) -> bool:
+        return self.blocking
+
+    
+class Floor(Tile):
+    def __init__(self):
+        super().__init__(FLOOR_ID, False)  # call parent constructor
+
+
+class Wall(Tile):
+    def __init__(self):
+        super().__init__(WALL_ID, True)  # call parent constructor
